3fd600b498557c992f7a182f3d335607b6b27a88
Nguyễn Thị Anh Thư <94220054+ntanh-thu@users.noreply.github.com>
2024-06-10 23:56:11 +0700

    admin sercurity

--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,4 @@
+import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 5;
@@ -7,6 +8,11 @@ export default function Nav() {
 
   const router = useRouter();
   const { pathname } = router;
+
+  async function logout() {
+    await signOut();
+    await router.push("/");
+  }
   return (
     <aside className="text-white p-4 pr-0">
       <Link href={"/"} className="flex gap-1 mb-4 mr-4">
@@ -113,6 +119,23 @@ export default function Nav() {
           </svg>
           Settings
         </Link>
+        <button onClick={logout} className={inactivceLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25"
+            />
+          </svg>
+          Log out
+        </button>
       </nav>
     </aside>
   );
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "../../../../lib/mongodb";
 
-export default NextAuth({
+const adminEmails = ["ntanhthu.156@gmail.com"];
+
+export const authOption = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
@@ -12,4 +14,24 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-});
+  callbacks: {
+    session: ({ session, token, user }) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOption);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOption);
+  if (!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw "not an admin";
+  }
+}
