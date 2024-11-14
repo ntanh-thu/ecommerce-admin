ed9c4912405db1fa75bbcab4ae29029c44144f86
Nguyễn Thị Anh Thư <94220054+ntanh-thu@users.noreply.github.com>
2024-11-15 00:40:23 +0700

    add constans for path name and name page

---
+++ constants/pageName.js
@@ -0,0 +1,9 @@
+const pages = {
+  "/": "Dashboard",
+  "/products": "Product",
+  "/categories": "Categories",
+  "/orders": "Order",
+  "/users": "Users",
+};
+
+export { pages };
---
+++ constants/pathName.js
@@ -0,0 +1,7 @@
+const HOME = "/";
+const PRODUCTS = "/products";
+const CATEGORIES = "/categories";
+const ORDERS = "/orders";
+const USERS = "/users";
+
+export { HOME, PRODUCTS, CATEGORIES, ORDERS, USERS };
